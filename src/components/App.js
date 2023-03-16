333286dcfa09913add24656f5f62a4c8df440a75
Evans <evansroysir@gmail.com>
2023-03-16 12:52:53 +0300

    Add header component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+
+      <Header blogName={blogData.name} />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.blogName}</h1>
+    </header>
+  );
+}
+export default Header;
